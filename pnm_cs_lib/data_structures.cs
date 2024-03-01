31b26fae318948d60071076c2214cb7e9fa8d70e
Luka Grubić <luka.grubic.ck@gmail.com>
2024-03-01 15:24:10 +0100

    Created pnm_cs_lib; Started implementing data_structures, utility_common

---
+++ pnm_cs_lib/data_structures.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections.Generic;
+
+
+
+public class RGB_PixelValues
+{
+    public int R { get; set; }
+    public int G { get; set; }
+    public int B { get; set; }
+
+    public RGB_PixelValues()
+    {
+        R = 0;
+        G = 0;
+        B = 0;
+    }
+
+    public RGB_PixelValues(int r, int g, int b)
+    {
+        R = r;
+        G = g;
+        B = b;
+    }
+
+    public RGB_PixelValues(int value)
+    {
+        R = value;
+        G = value;
+        B = value;
+    }
+} // !public class RGB_PixelValues
+
+public class RGB_PixelValues_List : List<RGB_PixelValues> { }
+
+
+
+public class Grey_PixelValues
+{
+    public int Value { get; set; }
+
+    public Grey_PixelValues()
+    {
+        Value = 0;
+    }
+
+    public Grey_PixelValues(int value)
+    {
+        Value = value;
+    }
+} // !public class Grey_PixelValues
+
+public class Grey_PixelValues_List : List<Grey_PixelValues> { }
---
+++ pnm_cs_lib/utility_common.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections.Generic;
+
+public static class pnm_cs_utility_common
+{
+
+
+
+    public static int ConvertToGreyscaleValue(int r, int g, int b)
+    {
+        return (int)((r * 0.299f) + (g * 0.587f) + (b * 0.114f));
+    }
+
+
+
+    public static int ConvertToGreyscaleValue(RGB_PixelValues rgb_pixelValues)
+    {
+        return ConvertToGreyscaleValue(rgb_pixelValues.R, rgb_pixelValues.B, rgb_pixelValues.B);
+    }
+
+
+
+    public static Grey_PixelValues_List ConvertToGreyscaleValueList(RGB_PixelValues_List in_list)
+    {
+        Grey_PixelValues_List out_list = new Grey_PixelValues_List();
+
+        foreach(RGB_PixelValues pixel in in_list)
+        {
+            out_list.Add(new Grey_PixelValues(ConvertToGreyscaleValue(pixel)));
+        }
+
+        return out_list;
+    }
+
+
+
+    public static RGB_PixelValues_List GetRGBListFromGreyscaleList(Grey_PixelValues_List in_list)
+    {
+        RGB_PixelValues_List out_list = new RGB_PixelValues_List();
+
+        foreach(Grey_PixelValues pixel in in_list)
+        {
+            out_list.Add(new RGB_PixelValues(pixel.Value));
+        }
+
+        return out_list;
+    }
+
+
+
+} // !namespace pnm_cs_lib_utility
